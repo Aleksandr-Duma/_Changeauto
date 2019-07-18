6bbf4b1ee7a83a039c04ec0d95e0ae9b106a0aa7
Aleksandr_Duma <Dumaweb9@gmail.com>
2019-07-18 22:11:43 +0300

    Second commit day 2

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	var dropdown = function(){
+		var data = $(".dropdown").attr("data-dropdown");
+
+		$(".dropdown-head").on("click", function(){
+			if(data === "close"){
+				$(".dropdown-body").slideUp();
+				if($(this).hasClass("active")){
+					$(this).toggleClass("active");
+				}
+				else{
+					$(".dropdown-head").removeClass("active");
+					$(this).toggleClass("active");
+				}
+			}
+			else{
+				$(this).toggleClass("active");
+			}
+			$(this).next(".dropdown-body").not(".animated").slideToggle();
+		});
+	}
+
+dropdown();
+})
